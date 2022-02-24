742485009f4c7dc6a14940fe7cd315e999de1a84
VinaySavla <savlavinay77@gmail.com>
2022-02-24 13:16:27 +0530

    changed font to openSans

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static final ThemeData? theme = ThemeData(
     colorSchemeSeed: Colors.green,
+    fontFamily: 'OpenSans',
   );
 
   static final ThemeData? darkTheme = ThemeData(
     brightness: Brightness.dark,
     colorSchemeSeed: Colors.green,
+    fontFamily: 'OpenSans',
   );
 
   static const ThemeMode? themeMode = ThemeMode.system;
