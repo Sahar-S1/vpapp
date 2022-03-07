db20bd9941669907cb30d4a631f2a7e19991dce4
anurag-393 <anuragd393.03@gmail.com>
2022-03-07 21:50:48 +0530

    Adapted new design

--- lib/pages/blogs.dart
+++ lib/pages/blogs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vpapp/components/blogs/blog_list.dart';
+import 'package:vpapp/components/common/page_template.dart';
 
 class BlogsPage extends StatelessWidget {
   static const routeName = 'blogs';
@@ -9,10 +10,11 @@ class BlogsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Blogs'),
+      appBar: AppBar(),
+      body: PageTemplate(
+        header: 'Blogs',
+        child: BlogsList(),
       ),
-      body: BlogsList(),
     );
   }
 }
