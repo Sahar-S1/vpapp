77a80c1163cfa401b85d3b1097005205e295754e
anurag-393 <anuragd393.03@gmail.com>
2022-03-07 21:48:29 +0530

    Cleanup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flutter/material.dart';
 import 'package:vpapp/app.dart';
 import 'package:vpapp/init.dart';
-import 'package:vpapp/services/blog.dart';
-import 'package:vpapp/services/club.dart';
 
 void main() async {
+  // Initialize App
   await init();
 
-  if (kDebugMode) {
-    print(await GetIt.instance.get<BlogService>().getAll());
-  }
+  // Launch App
   const app = App();
   runApp(app);
 }
