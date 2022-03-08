48801789dd7ed1bdba3487faf0fe9ba97e62eacc
Sahil Makhijani <sahil.makhijani@gmail.com>
2022-03-08 22:33:57 +0530

    Use aws server

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   static const String title = 'Vidyalankar Polytechnic';
-  static const String directusEndpoint = 'https://cms.vp.appsinfra.in/';
+  static const String directusEndpoint = 'https://aws.cms.vp.appsinfra.in/';
   static const String directusAssetsEndpoint = directusEndpoint + 'assets';
   static const String logoUrl =
       directusAssetsEndpoint + '/' + '1df44888-40db-41fe-a4f9-b42062954dd4';
