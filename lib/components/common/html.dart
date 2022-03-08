71e647eebb8754968315321f8d59950fe373107c
Sahil Makhijani <sahil.makhijani@gmail.com>
2022-03-08 19:41:43 +0530

    Show Image Viewer on Image Tap in HTML

--- lib/components/common/html.dart
+++ lib/components/common/html.dart
@@ -1,5 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
-import 'package:html/dom.dart' as dom;
 import 'package:flutter_html/flutter_html.dart' as flutter_html;
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,13 +13,16 @@ class Html extends StatelessWidget {
   Widget build(BuildContext context) {
     return flutter_html.Html(
       data: data,
-      onLinkTap: (
-        String? url,
-        flutter_html.RenderContext context,
-        Map<String, String> attributes,
-        dom.Element? element,
-      ) {
+      onLinkTap: (url, context, attributes, element) {
         if (url != null) launch(url);
+      },
+      onImageTap: (url, context, attributes, element) {
+        if (url != null) {
+          showImageViewer(
+            context.buildContext,
+            CachedNetworkImageProvider(url),
+          );
+        }
       },
     );
   }
