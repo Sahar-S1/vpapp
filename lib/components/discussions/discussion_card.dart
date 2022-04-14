bcd79548079cbffd2b81f90057590fe16a648868
Sahil Makhijani <sahil.makhijani@gmail.com>
2022-04-14 21:13:31 +0530

    Fixed discussion avatar and added number of messages

--- lib/components/discussions/discussion_card.dart
+++ lib/components/discussions/discussion_card.dart
@@ -22,39 +22,80 @@ class DiscussionCard extends StatelessWidget {
         ),
       ),
       margin: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        child: Row(
-          children: [
-            CircleAvatar(
-              backgroundImage: CachedNetworkImageProvider(
-                discussion.userCreated.avatarUrl,
-              ),
-            ),
-            const SizedBox(width: 12),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    discussion.title,
-                    style: theme.textTheme.titleMedium?.copyWith(
-                      color: theme.colorScheme.primary,
+      child: IntrinsicHeight(
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: ClipOval(
+                  child: SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: CachedNetworkImage(
+                      fit: BoxFit.contain,
+                      imageUrl: discussion.userCreated.avatarUrl,
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) {
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: downloadProgress.progress,
+                          ),
+                        );
+                      },
+                      errorWidget: (context, url, error) {
+                        return const Center(
+                          child: Icon(Icons.error),
+                        );
+                      },
                     ),
                   ),
-                  const SizedBox(height: 12),
-                  Text(
-                    discussion.userCreated.name,
-                    style: theme.textTheme.labelMedium,
+                ),
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      discussion.title,
+                      style: theme.textTheme.titleMedium?.copyWith(
+                        color: theme.colorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      discussion.userCreated.name,
+                      style: theme.textTheme.labelMedium,
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(discussion.dateCreated),
+                      style: theme.textTheme.caption,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 12),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.message,
+                    color: theme.colorScheme.onPrimaryContainer,
+                    size: theme.textTheme.caption?.fontSize,
                   ),
+                  const SizedBox(width: 4),
                   Text(
-                    DateFormat.yMMMd().format(discussion.dateCreated),
+                    discussion.comments.length <= 99
+                        ? NumberFormat('000').format(discussion.comments.length)
+                        : '99+',
                     style: theme.textTheme.caption,
                   ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
